08d9671f2494d66d6f714d4317ad042066c9194e
akietit1001 <ndakiet1001@gmail.com>
2022-11-26 15:27:16 +0700

    first commit

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,14 +4,14 @@ import 'firebase/analytics';
 import 'firebase/auth';
 import 'firebase/firestore';
 
-var firebaseConfig = {
-  apiKey: 'AIzaSyBRpDZ63mVfrzmzTjHFjRssP4cOvw_jc04',
-  authDomain: 'chat-app-c975a.firebaseapp.com',
-  projectId: 'chat-app-c975a',
-  storageBucket: 'chat-app-c975a.appspot.com',
-  messagingSenderId: '942648297216',
-  appId: '1:942648297216:web:d8c4321ba778c8808f95e5',
-  measurementId: 'G-01WLMKYFFM',
+const firebaseConfig = {
+    apiKey: "AIzaSyDMv0tvCvtf0T2-6OOUuG0O4lcEHh6M47k",
+    authDomain: "chat-app-a9b77.firebaseapp.com",
+    projectId: "chat-app-a9b77",
+    storageBucket: "chat-app-a9b77.appspot.com",
+    messagingSenderId: "974941698508",
+    appId: "1:974941698508:web:ff90fef907eeac8c9a0569",
+    measurementId: "G-9CKCYX008R"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -21,8 +21,8 @@ const auth = firebase.auth();
 const db = firebase.firestore();
 
 if (window.location.hostname === 'localhost') {
-  // auth.useEmulator('http://localhost:9099');
-  // db.useEmulator('localhost', '8080');
+    // auth.useEmulator('http://localhost:9099');
+    // db.useEmulator('localhost', '8080');
 }
 
 export { db, auth };
